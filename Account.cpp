2187c9687dde0940a8a4e4dcab721fbb3251bd12
Klivree <68071841+Klivree@users.noreply.github.com>
2020-07-23 16:50:55 -0400

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -3,23 +3,16 @@
 
 using namespace std;
 
-Account::Account()
-{
-    balance = 0.0;
-}
-
-int Account::setAccount(double balance_)
+Account::Account(double& balance_)
 {
     if (balance_ >= 0.0)
     {
         balance = balance_; // Assigning local variable value to the data member
-        return balance;
     }
     else
     {
         balance = 0.0; // Error message when the balance is negative
-        cout << "Error : The initial balance was invalid. Current balance to 0.0" << endl; 
-        return balance;
+        cout << endl << "Error : The initial balance was invalid. Current balance to 0.0" << endl;
     }
 }
 
@@ -28,12 +21,12 @@ double Account::getBalance()
     return balance; // Chain function calls 
 }
 
-void Account::credit(double amount)
+void Account::credit(double& amount)
 {
     balance += amount; // Add the amount to the balance of the account balance
 }
 
-bool Account::debit(double amount)
+bool Account::debit(double& amount)
 {
     if (balance >= amount) // Verify if there are enough money in the Account
     {
@@ -42,11 +35,8 @@ bool Account::debit(double amount)
     }
     else
     { // Error message because there is not enough money
-        cout << "Debit's amount exceeds account's balance. The balance has not been changed" << endl; 
+        cout << "Debit's amount exceeds account's balance. The balance has not been changed." << endl; 
         return false;
     }
     
 }
-
-Account::~Account() // Destructor
-{}
