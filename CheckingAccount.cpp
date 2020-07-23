f02e44fd74b0891fe3008e97a15999ae5383007c
Klivree <68071841+Klivree@users.noreply.github.com>
2020-07-23 13:41:38 -0400

    Update CheckingAccount.cpp

--- CheckingAccount.cpp
+++ CheckingAccount.cpp
@@ -8,24 +8,25 @@ CheckingAccount::CheckingAccount()
     tax = 0;
 }
 
-CheckingAccount::CheckingAccount (double balance, double tax) : Account(balance)
+void CheckingAccount::setCheckingAccount (double balance_, double tax_) // Creating a Checking account, derived from Account class
 {
-    this->tax = tax;
+    Account::setAccount(balance_); // set the new balance with the constructor
+    tax = tax_;
 }
 
-void CheckingAccount::credit(double amount)
+void CheckingAccount::credit(double amount) // Crediting an amount deducting the taxes
 {
     Account::credit(amount - tax);
 }
 
-double CheckingAccount::debit(double amount)
+double CheckingAccount::debit(double amount) // Debiting the amount with taxes if it is possible
 {
     amount += tax;
-    if (Account::debit(amount + tax))
+    if (Account::debit(amount + tax)) // Verifying if there are enough money in the Account
     {
         return balance;
     }
-    else
+    else // There are not enough money in the Account
     {
         Account::debit(amount + tax);
         return balance;
