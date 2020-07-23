a767e7a55cf6826a3e1550cec21988ad5244217b
Klivree <68071841+Klivree@users.noreply.github.com>
2020-07-23 16:54:22 -0400

    Update Assignment_2.cpp

--- Assignment_2.cpp
+++ Assignment_2.cpp
@@ -3,48 +3,39 @@
 #include "CheckingAccount.h"
 
 using namespace std;
-void transaction(Account);
+void transaction(CheckingAccount&, double&);
 
 int main()
 {
-	Account Account;
-	double money, rate, tax, balance{ 0 };
+	double money, rate{ 0 }, tax{ 0 }, amount{ 0 };
 	cout << "I will, through this programm, ask you to enter some values relative to you account, if you don't mind."
 		<< "\nI am creating an account or you.\nHow much money do you have in your account ?" << endl;
 	cin >> money;
-	Account.setAccount(money);
-	cout << "Then your initial account's balance is: $" << Account.getBalance() << endl;
-	transaction(Account);
-
-
-	cout << "I will now calculate the interests of a saving's account for you, depending on the rate of your bank.";
-	SavingsAccount Savings_Account;
-	balance = Savings_Account.getBalance();
-	cout << "The balance of your account is still " << Savings_Account.getBalance() << endl
-		<< "What is the current interest rate of your bank ?" << endl;
+	Account* account = new Account(money);
+	cout << "\nThen your initial account's balance is: $" << account->getBalance() << endl;
+	transaction(dynamic_cast <CheckingAccount*>(account), amount);
+	money = amount;
+	cout << "\nI will now calculate the interests of a saving's account for you, depending on the rate of your bank." << endl
+	<< "What is the current interest rate (in %) of your bank ?" << endl;
 	cin >> rate;
-
-	Savings_Account.setSavingsAccount(balance, rate);
-	cout << "The interests of the saving's account you have is$ " << Savings_Account.calculateInterest();
-
-	CheckingAccount Checking_Account;
-	balance = Checking_Account.getBalance();
-	cout << "I will now create an other account for you."
-		<< "\nThis account has fees per transaction."
+	SavingsAccount savings_account(money, rate);
+	cout << "\nThe balance of your account is still $" << savings_account.getBalance() << endl;
+	cout << "The interests of the saving's account you have is$ " << savings_account.calculateInterest();
+	
+	cout << "\n\nI will now create an other account for you."
+		<< "\nThis account has fees per transaction. "
 		<< "I will calculate everything for you, don't worry." << endl
-		<< "Please enter the amount of the tax the banks takes from your account" << endl;
+		<< "Please enter the amount (in $) of the taxes the banks takes from your account." << endl;
 	cin >> tax;
-	Checking_Account.setCheckingAccount(balance, tax);
-	cout << "The intial balance of the Cheking account is$ " << Checking_Account.getBalance() << endl;
-	transaction(Checking_Account);
-
+	CheckingAccount checking_account(money, tax);
+	cout << "\nThe intial balance of the Cheking account is$ " << checking_account.getBalance() << endl;
+	transaction(checking_account, amount);
 	return 0;
 }
 
-void transaction(Account Account)
+void transaction(CheckingAccount& account, double& amount)
 {
 	string input;
-	double amount;
 
 	cout << "\nIf you want to make a debit or a credit in your account, please enter 'credit' or 'debit." << endl
 		<< "If you don't want to make any transactions at all, please enter 'stop'.\n";
@@ -55,24 +46,25 @@ void transaction(Account Account)
 		{
 			cout << "Ok, then enter the amount I have to credit in your account.\n";
 			cin >> amount;
-			Account.credit(amount);
-			cout << "The new balance in your account is: $" << Account.getBalance() << endl;
-			cout << "Do you want to make an other transaction ?\nPlease enter credit, debit, or write stop to finish all the transactions.\n";
+			account.credit(amount);
+			cout << "The new balance in your account is: $" << account.getBalance() << endl;
+			cout << "\nDo you want to make an other transaction ?\nPlease enter credit, debit, or write stop to finish all the transactions.\n";
 			cin >> input;
 		}
 		else if (input == "debit")
 		{
 			cout << "Ok, then enter the amount I have to debit from your account.\n";
 			cin >> amount;
-			Account.debit(amount);	
-			cout << "The new balance in your account is: $" << Account.getBalance() << endl;
+			account.debit(amount);
+			cout << "The new balance in your account is: $" << account.getBalance() << endl;
 			cout << "Do you want to make an other transaction ?\nPlease enter credit, debit, or write stop to finish all the transactions.\n";
 			cin >> input;
 		}
-		else
+		else if (input != "stop")
 		{
 			cout << "I don't understand what you wrote, please enter either debit, credit or stop to let me know what to do.\n";
 			cin >> input;
 		}
 	} while (input != "stop");
+	amount = account.getBalance();
 }
