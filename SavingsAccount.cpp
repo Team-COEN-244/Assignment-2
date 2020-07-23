76536f97ec28f1d4bc55e90c30faf74dd47259b0
Klivree <68071841+Klivree@users.noreply.github.com>
2020-07-23 13:43:02 -0400

    Update SavingsAccount.cpp

--- SavingsAccount.cpp
+++ SavingsAccount.cpp
@@ -8,15 +8,16 @@ SavingsAccount::SavingsAccount()
     rate = 0.0;
 }
 
-SavingsAccount::SavingsAccount(double balance, double rate_ ) : Account(balance) // creating a checking account, derived from Account class
+void SavingsAccount::setSavingsAccount(double balance, double rate_) // creating a savings account, derived from Account class
 {
-    this->rate = rate_; // Assigns the local value for rate to the data member
+    Account::setAccount(balance);
+    rate = rate_; // Assigns the local value for rate to the data member
 }
 
 double SavingsAccount :: calculateInterest()
 {
     double interest; // Creating a double variable
-    interest = balance * rate; // Calulate the interest in this account de
+    interest = balance * rate; // Calulate the interest in this account
     return interest;
 }
 
